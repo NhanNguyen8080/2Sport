b88e73a18434ccb8ff11dabd1b33d99102ff0512
hhathaoo <hathao01112002@gmail.com>
2024-05-22 13:14:07 +0700

    Add route product, productv2
    Add route product, productv2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,8 +4,6 @@ import './App.css'
 import Header from './layouts/Header'
 import LandingPage from './pages/LandingPage';
 import Footer from './layouts/Footer';
-import ProductPage from './pages/ProductPage';
-import Productv2Page from './pages/Productv2Page';
 
 function App() {
   return (
@@ -13,8 +11,6 @@ function App() {
      <Header/>
      <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/product" element={<ProductPage />} />
-          <Route path="/productv2" element={<Productv2Page />} />
      </Routes>
      <Footer/>
     </>
