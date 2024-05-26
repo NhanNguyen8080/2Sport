5c6bdb59f69ec14189a8a27fe7fe4b6d7374969b
LinhDTT9502 <duongthitruclinh257@gmail.com>
2024-05-26 22:59:04 +0700

    feat user function: signin, signup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,16 +6,21 @@ import LandingPage from './pages/LandingPage';
 import ProductPage from './pages/ProductPage';
 import Productv2Page from './pages/Productv2Page';
 import Footer from './layouts/Footer';
+import ManageAccount from './pages/ManageAccount';
+import { BreadcrumbsDefault } from './layouts/BreadcrumbsDefault';
 
 function App() {
   return (
     <>
      <Header/>
+     <div className="pt-28">
+      {/* <BreadcrumbsDefault/> */}
      <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/product" element={<ProductPage />} />
           <Route path="/productv2" element={<Productv2Page />} />
      </Routes>
+     </div>
      <Footer/>
     </>
   )
---
+++ Frontend/src/pages/ManageAccount.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+function ManageAccount() {
+
+    return (
+        <>
+            <div className="pt-28">
+                <h2> Edit Your Profile</h2>
+            </div>
+        </>
+    );
+}
+
+export default ManageAccount;
