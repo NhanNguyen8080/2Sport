a24aa7143806dc4321fa06f1dace0d5cc5e2161a
NhanNguyen8080 <boiboii162122@gmail.com>
2024-06-20 18:33:17 +0700

    Fix bug

--- Backend/2Sport_BE.Core/Models/Product.cs
+++ Backend/2Sport_BE.Core/Models/Product.cs
@@ -33,7 +33,7 @@ namespace _2Sport_BE.Repository.Models
         public int? ClassificationId { get; set; }
         public string ProductCode { get; set; }
 
-        public virtual Brand Brand { get; set; }
+        public virtual Brand? Brand { get; set; }
         public virtual Category Category { get; set; }
         public virtual Sport Sport { get; set; }
         public virtual Classification Classification { get; set; }
